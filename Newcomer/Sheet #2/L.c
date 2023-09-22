1ac6b2b1c8c83ed5b66678f8fe2945722bd84059
Farhadul Islam <140002534+elfaradio@users.noreply.github.com>
2023-09-23 01:15:01 +0600

    L.c

---
+++ Newcomer/Sheet #2/L.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int i,a,b,g;
+    scanf("%d%d",&a,&b);
+    for(i=1;i<=a&&i<=b;i++)
+    {
+        if(a%i==0&&b%i==0)
+        {
+            g=i;
+        }
+    }
+    printf("%d\n",g);
+
+    return 0;
+}
