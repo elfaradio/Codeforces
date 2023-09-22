23836d77f519520a5341f29e74e08175fcc65c99
Farhadul Islam <140002534+elfaradio@users.noreply.github.com>
2023-09-23 01:13:45 +0600

    F.c

---
+++ Newcomer/Sheet #2/F.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int i=1;
+    int a;
+    scanf("%d",&a);
+    while(i<=12) {
+            printf("%d * %d = %d\n",a,i,i*a);
+            i++;
+
+    }
+
+    return 0;
+}
