c748c12aa6432f3a473b20c6e02ba07be35e31b0
Farhadul Islam <140002534+elfaradio@users.noreply.github.com>
2023-10-08 18:54:20 +0600

    Divisibility Problem.c

---
+++ 800 Rating/Divisibility Problem.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int t;
+    scanf("%d", &t);
+
+    while (t--) {
+        int a, b;
+        scanf("%d %d", &a, &b);
+
+        if (a % b == 0) {
+            printf("0\n");
+        } else {
+            printf("%d\n", b - (a % b));
+        }
+    }
+
+    return 0;
+}
