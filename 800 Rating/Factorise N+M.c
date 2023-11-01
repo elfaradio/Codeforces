7b7b0f3a067971b69e832b131a0e2d332e8b919f
Farhadul Islam <140002534+elfaradio@users.noreply.github.com>
2023-11-01 20:29:56 +0600

    Factorise N+M.c

---
+++ 800 Rating/Factorise N+M.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int t;
+    scanf("%d", &t);
+
+    while (t--)
+    {
+        int n;
+        scanf("%d", &n);
+        printf("%d\n", n);
+    }
+
+    return 0;
+}
